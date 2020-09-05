26f0175e555040f93d15a3ec04e905341da34370
Robfer28 <robfer28@gmail.com>
2020-09-04 23:44:27 -0400

    adding onClick funtions to calculator buttons

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,26 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Calculator.css";
 
-function Calculator() {
+function Calculator(usage) {
   return (
     <div className="Calculator">
-      <button className="Keypad">+</button>
-      <button className="Keypad">-</button>
-      <button className="Keypad">x</button>
-      <button className="Keypad">÷</button>
-      <button className="Keypad">9</button>
-      <button className="Keypad">8</button>
-      <button className="Keypad">7</button>
-      <button className="Keypad">6</button>
-      <button className="Keypad">5</button>
-      <button className="Keypad">4</button>
-      <button className="Keypad">3</button>
-      <button className="Keypad">2</button>
-      <button className="Keypad">1</button>
-      <button className="Keypad">0</button>
-      <button className="Keypad">.</button>
-      <button className="Keypad AC">AC</button>
-      <button className="Keypad Equals">=</button>
+      <button
+        className="Key"
+        name="+"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        +
+      </button>
+      <button
+        className="Key"
+        name="-"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        -
+      </button>
+      <button
+        className="Key"
+        name="x"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        x
+      </button>
+      <button
+        className="Key"
+        name="÷"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        ÷
+      </button>
+      <button
+        className="Key"
+        name="9"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        9
+      </button>
+      <button
+        className="Key"
+        name="8"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        8
+      </button>
+      <button
+        className="Key"
+        name="7"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        7
+      </button>
+      <button
+        className="Key"
+        name="6"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        6
+      </button>
+      <button
+        className="Key"
+        name="5"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        5
+      </button>
+      <button
+        className="Key"
+        name="4"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        4
+      </button>
+      <button
+        className="Key"
+        name="3"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        3
+      </button>
+      <button
+        className="Key"
+        name="2"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        2
+      </button>
+      <button
+        className="Key"
+        name="1"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        1
+      </button>
+      <button
+        className="Key"
+        name="0"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        0
+      </button>
+      <button
+        className="Key"
+        name="."
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        .
+      </button>
+      <button
+        className="Key AC"
+        name="AC"
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        AC
+      </button>
+      <button
+        className="Key Equals"
+        name="="
+        onClick={(e) => usage.onClick(e.target.name)}
+      >
+        =
+      </button>
     </div>
   );
 }
